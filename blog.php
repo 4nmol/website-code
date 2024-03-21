931d1ed400be570d1c3fc9010564f5e43f55e5db
4nmol <164221678+4nmol@users.noreply.github.com>
2024-03-21 08:17:33 -0700

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,153 @@
+<html lang="en">
+<head><style>
+@keyframes blink {
+  0% { opacity: 1; }
+  50% { opacity: 0; }
+  100% { opacity: 1; }
+}
+.blink-text {
+font-family: 'Lucida Calligraphy' ;/* Default font */
+color:red;
+  animation: blink 1s infinite; /* Blinking animation */
+font-size:24px;
+}
+.eight-color-line {
+    display: flex;
+    height: 20px; /* Adjust height as needed */
+color: purple;
+    font-family: 'Lucida Calligraphy' ;/* Default font */
+padding: 10px; /* Padding around the links */
+    size: 16px; /* Default font size */
+    border-radius: 5px;
+/* Align child container horizontally */
+  align-items: center;
+  }
+
+  .color-block {
+    flex: 1;
+  }
+
+  /* Colors for the blocks */
+  .color-block:nth-child(1) {  background-color: #FF007F;}
+  .color-block:nth-child(2) { background-color: #FF007F; }
+  .color-block:nth-child(3) { background-color: #FF007F; }
+  .color-block:nth-child(4) {background:linear-gradient(to right,black -10% ,skyblue 50%,black 110%);border: 3px solid white;}}
+  .color-block:nth-child(5) { background-color: #FF007F; }
+  .color-block:nth-child(6) { background-color: #FF007F; }
+  .color-block:nth-child(7) { background-color: #FF007F; }
+  .color-block:nth-child(8) { background-color: #FF007F; }
+
+.container {
+    display: flex;
+    height: 100vh; /* Full viewport height */
+}
+
+.fixed {
+    width: 100%;/* Half of the page width */
+    position: fixed;
+  
+  overflow: auto; /* Enable scrolling if content overflows */
+}
+        
+.title {
+    font-family:'Blackadder ITC', serif; 
+    color: #6A0DAD;
+    text-align: center;
+    margin-top: 50px; 
+    background:linear-gradient(to right,#FF69B4 25%, white 50%,#FF69B4 100%);
+font-size:50px;
+        }
+.subtitle {
+            font-family: 'Footlight MT Light'; 
+            color: #ffffff; /* White text color */
+            text-align: center;
+         background:linear-gradient(to right,black 0% ,#6A0DAD 50%,black 110%);
+            padding: 15px; /* Add padding to create space around the text */
+ border: 5px solid white;
+        }
+.navigation {
+    color: purple;
+    font-family: 'Lucida Calligraphy';/* Default font */
+    background:linear-gradient(to right, 
+            yellow 0%, 
+            yellow 14.28%, 
+            orange 28.56%, 
+            red 42.84%, 
+            orange 57.12%, 
+            yellow 71.4%, 
+            yellow 85.68%, 
+            yellow 100%
+        );
+    padding: 10px; /* Padding around the links */
+    size: 16px; /* Default font size */
+    border-radius: 5px;
+}
+.styled-heading {
+   color: #6A0DAD; /* Font color */
+    height:50px;
+    width:100px;
+    background: linear-gradient(235deg, violet, white);
+    font-family: 'Footlight MT Light'; /* Font family */
+    font-size: 36px; /* Font size */
+    padding: 10px; /* Padding around the heading */
+}
+.backgrounda{
+    border:3px solid #FF007F;
+    background: linear-gradient(135deg,red,orange,white,yellow,white,orange,red);
+    
+}
+.fonta {
+    font-size:25px;
+    width:800px;
+    color:  #654321;/* Font color */
+    font-family: "Calisto MT", serif; /* Font family */
+}
+.backgroundb{
+    border: 2px solid #6A0DAD;
+  
+    background-color: #FF007F;
+}
+    </style>
+   
+</head>
+    <body>
+        <div class="backgrounda" class="container">
+        <div class="fixed">
+            <div class="backgroundb">
+            <br>
+                <header>
+                    <h1 class="title"><u>The Donation Nation</u></h1>
+                        <h2 class="subtitle">.•:*.•:*¨༺༻ ANIMAL RESCUE CHARITY༺༻¨*:•.*:•.</h2>
+                            <nav>
+                                <div class="eight-color-line">
+  <div class="color-block"><a href="checkout.php"><b>&nbsp;&nbsp;Home&nbsp;&nbsp;</b></a></div>
+  <div class="color-block"><a href="about.php">&nbsp;&nbsp;About&nbsp;&nbsp;</a></div>
+  <div class="color-block"><a href="contact.php">&nbsp;&nbsp;Contact&nbsp;&nbsp;</a></div>
+  <div class="color-block"><a href="blog.php">&nbsp;&nbsp;◉ Blogs&nbsp;&nbsp;</a></div>
+  <div class="color-block"></div>
+  <div class="color-block"></div>
+  <div class="color-block"></div>
+  <div class="color-block"></div>
+</div>
+                            </nav>
+                </header>
+                <br>
+                </div>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div >
+            <h1 class="styled-heading"><u>BLOGS</u></h1>
+                <p class="fonta"><b><u>1)Donate for sick strays- Help Animals in India</u></b><br>
+                Every month is a way in helping more and more animals in need. Our area of activity is and number of rescue cases are increasing each day. An average of 3-4 stray animals are getting  admissions in single day at our veterinary hospital. We have been tirelessly working to make the life of strays better. By providing medical care, food and creating awareness among people, their lives are better. Though it is sad many animals were sick, we are happy that we could help them when they needed it the most. With your continued support, we have been able to...<br><br><br>
+
+                <b><u>2)Staff wanted for animal rescue Organization- Pondicherry</u></b><br>
+                Are you a compassionate person who would like to turn your passion into a profession? Join our team to help stray animals that need your compassionate heart. We urgently require animal handlers cum helpers ( 3 vacancies ) for our animals rescue centre and hospital. Help the strays by being a part of the rescue, treatment and welfare of animals.</p><br><br><br><br><br><br>
+<div class="blink-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Thanks to visit us</div>
+
+<br><br><br><br><br>
+
+
+
+        </div >
+    </body>
+</html>
